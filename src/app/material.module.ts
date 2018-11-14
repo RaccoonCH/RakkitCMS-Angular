ee3cbdc6ff4b30e9cb255988bb86dd19f62519ef
Xavier Gómez <xavier@raccoon.ch>
2018-11-14 14:04:44 +0100

    Adjusts material.module.ts and adds custom theme

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,44 +1,23 @@
 import { NgModule } from '@angular/core';
 import {
-    MatButtonModule,
-    MatMenuModule,
-    MatIconModule,
-    MatCardModule,
-    MatSidenavModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatTooltipModule,
+    MatTabsModule,
     MatToolbarModule
 } from '@angular/material';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     imports: [
-        MatButtonModule,
-        MatMenuModule,
-        MatIconModule,
-        MatCardModule,
-        MatSidenavModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatTooltipModule,
+        MatTabsModule,
         MatToolbarModule,
 
         BrowserAnimationsModule
+        // NoopAnimationsModule
     ],
     exports: [
-        MatButtonModule,
-        MatMenuModule,
-        MatIconModule,
-        MatCardModule,
-        MatSidenavModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatTooltipModule,
-        MatToolbarModule,
-
-        BrowserAnimationsModule
+        MatTabsModule,
+        MatToolbarModule
     ]
 })
 export class MaterialModule { }
