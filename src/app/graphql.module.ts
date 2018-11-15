dbbc9582876d1bd9194f9735ae5b6f1bba4edd4a
Owen Gombas <owen.gombas@gmail.com>
2018-11-15 10:35:16 +0100

    API service

---
+++ src/app/graphql.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
+
+@NgModule({
+  exports: [ApolloModule, HttpLinkModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: null,
+      deps: [HttpLink],
+    },
+  ],
+})
+export class GraphQLModule {}
