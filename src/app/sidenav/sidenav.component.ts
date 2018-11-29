e6234427544cf6e800bfe0dc933d868380e5d132
Owen Gombas <owen.gombas@gmail.com>
2018-11-29 10:08:33 +0100

    Sheet class and bug fix

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component } from '@angular/core'
 import { SidenavService } from '../sidenav.service'
 import { Sheet } from '../../types'
 
@@ -9,7 +9,6 @@ import { Sheet } from '../../types'
 })
 export class SidenavComponent {
   private _sheets: Array<Sheet> = []
-  private _animationDuration = 400
 
   constructor(
     private _sidenavService: SidenavService
@@ -23,65 +22,34 @@ export class SidenavComponent {
     if (this._sheets.length <= 0) {
       return true
     }
-    if (this._sheets.length === 1 && this._sheets[0].leaving === true) {
+    if (this._sheets.length === 1 && this._sheets[0].Leaving === true) {
       return true
     }
     return false
   }
 
-  closeLastSheet() {
+  async closeLastSheet() {
     const index = this._sheets.length - 1
     const lastSheet = this._sheets[index]
     const beforeLastSheet = this._sheets[index - 1]
-    if ((beforeLastSheet && !beforeLastSheet.disabling) || !beforeLastSheet) {
-      lastSheet.right = true
-      lastSheet.leaving = true
-
-      if (this._sheets.length > 1) {
-        beforeLastSheet.active = true
-      }
-
-      setTimeout(() => {
-        this._sheets.splice(index, 1)
-      }, this._animationDuration)
+    if ((beforeLastSheet && !beforeLastSheet.Disabling) || !beforeLastSheet) {
+      await Promise.all([
+        lastSheet.close(),
+        this._sheets.length > 1 && beforeLastSheet.enable()
+      ])
+      this._sheets.splice(index, 1)
     }
   }
 
   addSheet(rp: Object) {
     const sheetBefore = this._sheets[this._sheets.length - 1]
     if (sheetBefore) {
-      sheetBefore.active = false
-      sheetBefore.disabling = true
+      sheetBefore.disable()
     }
 
-    const newSheet = {
-      active: true,
-      right: true,
-      entering: true,
-      leaving: false,
-      disabling: false,
-      rp
-    }
-
+    const newSheet = new Sheet(rp)
     this._sheets.push(newSheet)
-
-    setTimeout(() => {
-      newSheet.right = false
-    })
-
-    setTimeout(() => {
-      newSheet.entering = false
-      if (sheetBefore) {
-        sheetBefore.disabling = false
-      }
-    }, this._animationDuration)
-  }
-
-  private getStyle(sheet: Sheet) {
-    return {
-      transition: `all ${this._animationDuration}ms`,
-      ...(sheet.active && sheet.right ? { right: '-100%' } : {})
-    }
+    newSheet.open()
   }
 
   private stringify(obj: Object) {
